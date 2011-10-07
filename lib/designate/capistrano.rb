8d8ccfa27090f97b0f091956e3bd1c8de55b9c2a
Noah Cantor <noah.cantor@forward.co.uk>
2011-10-07 13:52:14 +0100

    Added ability to handle subdomains when registering domains.

--- lib/designate/capistrano.rb
+++ lib/designate/capistrano.rb
@@ -9,9 +9,16 @@ Capistrano::Configuration.instance(:must_exist).load do
       roles.each do |role| 
         dns_names << role[1].instance_variable_get("@static_servers").first.instance_variable_get("@host")
       end
+
       zone = Designate::Client.new().find_or_create_zone()
-      zone.create_host('CNAME', "#{hostname}.#{zerigo_config[:domain]}", {:hostname => "#{hostname}-#{application}"})
       zone.create_host('CNAME', dns_names.first, {:hostname => hostname})
+      
+      if zerigo_config[:subdomain].nil?
+        zone.create_host('CNAME', "#{hostname}.#{zerigo_config[:domain]}", {:hostname => "#{hostname}-#{application}"})
+      else
+        subzone = Designate::Client.new().find_or_create_zone("#{zerigo_config[:subdomain]}.#{zerigo_config[:domain]}")
+        subzone.create_host('CNAME', "#{hostname}.#{zerigo_config[:domain]}", {:hostname => "#{hostname}"})
+      end
     end
     
     def zerigo_config()
