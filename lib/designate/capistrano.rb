b08760a0ac04333982a7fc1adce38bbd51a56ddb
Noah Cantor <noah.cantor@forward.co.uk>
2012-03-30 14:25:23 +0100

    Now with old host removal after creating the new host.

--- lib/designate/capistrano.rb
+++ lib/designate/capistrano.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), '../designate')
 require 'rubygems'
 require 'capistrano'
+require 'capify-ec2'
 
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :zerigo do
@@ -11,7 +12,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       roles.each do |role| 
         dns_names << role[1].instance_variable_get("@static_servers").first.instance_variable_get("@host")
       end
-
       zone = Designate::Client.new().find_or_create_zone()
       zone.create_host('CNAME', dns_names.first, {:hostname => hostname})
       
@@ -20,11 +20,31 @@ Capistrano::Configuration.instance(:must_exist).load do
       else
         subzone = Designate::Client.new().find_or_create_zone("#{zerigo_config[:subdomain]}.#{zerigo_config[:domain]}")
         subzone.create_host('CNAME', "#{hostname}.#{zerigo_config[:domain]}", {:hostname => "#{hostname}"})
+        cleanup_hosts(subzone, zone)
       end
     end
     
     def zerigo_config()
       YAML.load(File.new("config/zerigo.yml"))
+    end
+    
+    def cleanup_hosts(subzone, zone)
+      return if !zerigo_config[:host_prefix]
+      expired_hosts = get_expired_hosts(subzone)
+      delete_hosts(expired_hosts, subzone)
+      delete_hosts(expired_hosts, zone)
+    end
+    
+    def get_expired_hosts(subzone)
+      hostnames = CapifyEc2.new().desired_instances.select {|instance| instance.name.match(zerigo_config[:host_prefix])}.map {|instance| instance.name}
+      subzone.hosts.delete_if {|host| hostnames.find {|hostname| hostname == host.hostname} || 
+        !host.hostname.match(zerigo_config[:host_prefix])}.map {|host| host.hostname}
+    end
+    
+    def delete_hosts(expired_hosts, zone)
+      zone.hosts.delete_if {|host| !expired_hosts.find {|hostname| hostname == host.hostname}}
+      zone.hosts.each {|host| p "Host to delete: #{host.hostname}"; host.destroy}
+      #region-20120330-1112-00 should be deleted      
     end
   end
 end
--- lib/designate/version.rb
+++ lib/designate/version.rb
@@ -1,3 +1,3 @@
 module Designate
-  VERSION = "0.0.8.pre"
+  VERSION = "0.0.8.pre.3"
 end
